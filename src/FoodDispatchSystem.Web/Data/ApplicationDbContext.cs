27684048d0de206edd1630e172a2577b27fe0af0
Yader Baires <ymbaires90@gmail.com>
2026-09-09 14:23:36 -0600

    Configure Entity Framework Core and add initial database

---
+++ src/FoodDispatchSystem.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using FoodDispatchSystem.Web.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FoodDispatchSystem.Web.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(
+        DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Category> Categories { get; set; } = null!;
+}
