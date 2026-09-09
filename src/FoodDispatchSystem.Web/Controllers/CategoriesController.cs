b868d3e63b4c8d5333ab520887de25d1be475a69
Yader Baires <ymbaires90@gmail.com>
2026-09-09 15:19:02 -0600

    Implement category CRUD with soft delete

---
+++ src/FoodDispatchSystem.Web/Controllers/CategoriesController.cs
@@ -0,0 +1,116 @@
+﻿using FoodDispatchSystem.Web.Models;
+using FoodDispatchSystem.Web.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FoodDispatchSystem.Web.Controllers;
+
+public class CategoriesController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public CategoriesController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var categories = await _context.Categories
+            .OrderBy(c => c.Name)
+            .ToListAsync();
+
+        return View(categories);
+    }
+    public IActionResult Create()
+    {
+        return View(new Category());
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create(Category category)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Categories.Add(category);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        return View(category);
+    }
+
+    public async Task<IActionResult> Edit(int id)
+    {
+        var category = await _context.Categories.FindAsync(id);
+
+        if (category == null)
+        {
+            return NotFound();
+        }
+
+        return View(category);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(int id, Category category)
+    {
+        if (id != category.Id)
+        {
+            return NotFound();
+        }
+
+        if (!ModelState.IsValid)
+        {
+            return View(category);
+        }
+
+        var existingCategory = await _context.Categories.FindAsync(id);
+
+        if (existingCategory == null)
+        {
+            return NotFound();
+        }
+
+        existingCategory.Name = category.Name;
+        existingCategory.Description = category.Description;
+        existingCategory.IsActive = category.IsActive;
+
+        await _context.SaveChangesAsync();
+
+        return RedirectToAction(nameof(Index));
+    }
+
+    public async Task<IActionResult> Delete(int id)
+    {
+        var category = await _context.Categories.FindAsync(id);
+
+        if (category == null)
+        {
+            return NotFound();
+        }
+
+        return View(category);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(int id)
+    {
+        var category = await _context.Categories.FindAsync(id);
+
+        if (category == null)
+        {
+            return NotFound();
+        }
+
+        category.IsActive = false;
+
+        await _context.SaveChangesAsync();
+
+        return RedirectToAction(nameof(Index));
+    }
+}
